4e5baf0eb6452a06e0873c5e25ce969bdef5e613
chris <62446202+5xp@users.noreply.github.com>
2023-08-08 02:49:31 -0500

    feat: Add scrolling graph to synchronizer

--- scripts/hud/synchronizer.js
+++ scripts/hud/synchronizer.js
@@ -10,28 +10,50 @@ const COLORS = {
 	STOP: ['rgba(211, 24, 24, 1)', 'rgba(255, 87, 87, 1)']
 };
 
+const SYNC_STATES = {
+	SYNCED: 0,
+	UNSYNCED: 1,
+	NOMOVE: 2,
+	LOSS: 3
+};
+
+const GRAPH_COLORS = {
+	[SYNC_STATES.SYNCED]: COLORS.NEUTRAL[1],
+	[SYNC_STATES.UNSYNCED]: COLORS.LOSS[1],
+	[SYNC_STATES.NOMOVE]: COLORS.STOP[1],
+	[SYNC_STATES.LOSS]: COLORS.STOP[0]
+};
+
 const DEFAULT_BUFFER_LENGTH = 10;
 const DEFAULT_MIN_SPEED = 200;
 const DEFAULT_SETTING_ON = 1;
 const DEFAULT_SETTING_OFF = 0;
+const DEFAULT_GRAPH_BUFFER_LENGTH = 80;
+const DEFAULT_GRAPH_MODE = 1;
 
 class Synchronizer {
 	static panels = {
 		wrapper: $('#BarWrapper'),
 		segments: [$('#Segment0'), $('#Segment1'), $('#Segment2'), $('#Segment3'), $('#Segment4')],
+		background: $('#Background'),
 		container: $('#Container'),
 		needle: $('#Needle'),
-		stats: [$('#StatsUpper'), $('#StatsLower')]
+		stats: [$('#StatsUpper'), $('#StatsLower')],
+		graphContainer: $('#GraphContainer'),
+		graph: $('#SynchroGraph')
 	};
 
 	static rad2deg = 180 / Math.PI;
 	static deg2rad = 1 / this.rad2deg;
 	static indicatorPercentage = 90; // this value shows ~90% gain or better when strafe indicator touches needle
+	static strafeDirPercentage = 2 / 75;
 	static bIsFirstPanelColored = true; // gets toggled in wrapValueToRange()
 	static maxSegmentWidth = 25; // percentage of total element width
 	static firstPanelWidth = this.maxSegmentWidth;
 	static syncGain = 10; // scale how fast the bars move
 	static altColor = 'rgba(0, 0, 0, 0.0)';
+	static lastAngle = 0;
+	static lastSynchroDir = 1;
 
 	static onLoad() {
 		this.initializeSettings();
@@ -42,26 +64,41 @@ class Synchronizer {
 	static onUpdate() {
 		const lastMoveData = MomentumMovementAPI.GetLastMoveData();
 		const lastTickStats = MomentumMovementAPI.GetLastTickStats();
+		const viewAngle = MomentumPlayerAPI.GetAngles().y;
 
 		//zero buffers
 		this.addToBuffer(this.gainRatioHistory, 0);
 		this.addToBuffer(this.yawRatioHistory, 0);
 
 		const bValidWishMove = this.getSize(lastMoveData.wishdir) > 0.1;
-		const strafeRight = (bValidWishMove ? 1 : 0) * lastTickStats.strafeRight;
-		const direction = this.dynamicEnable === 1 ? strafeRight : 1;
+		const angleDelta = this.wrapValueToRange(this.lastAngle - viewAngle, -180, 180);
+		this.lastAngle = viewAngle;
+		const turnDir = angleDelta > 0 ? 1 : angleDelta < 0 ? -1 : 0;
+
+		const direction = this.dynamicEnable === 1 ? turnDir || this.lastSynchroDir : 1;
+		this.lastSynchroDir = direction;
 		const flip = this.flipEnable === 1 ? -1 : 1;
 
-		if (bValidWishMove && this.getSizeSquared(MomentumPlayerAPI.GetVelocity()) > Math.pow(this.minSpeed, 2)) {
+		if (this.getSizeSquared(MomentumPlayerAPI.GetVelocity()) > Math.pow(this.minSpeed, 2)) {
 			this.gainRatioHistory[this.interpFrames - 1] =
 				this.sampleWeight * this.NaNCheck(lastTickStats.speedGain / lastTickStats.idealGain, 0);
 
-			const ratio = this.displayMode > 2 ? 1 - lastTickStats.yawRatio : lastTickStats.yawRatio;
+			const ratio = this.displayMode > 2 ? 1 - lastTickStats.yawRatio : Math.abs(lastTickStats.yawRatio);
 			this.yawRatioHistory[this.interpFrames - 1] = this.sampleWeight * this.NaNCheck(ratio, 0);
 		}
 
 		const gainRatio = this.getBufferedSum(this.gainRatioHistory);
 		const yawRatio = this.getBufferedSum(this.yawRatioHistory);
+
+		if (this.shouldDraw()) {
+			this.addToBufferFront(this.graphHistory, {
+				ratio: yawRatio,
+				flow: direction * flip,
+				gainRatio: gainRatio,
+				syncState: this.getSyncState(bValidWishMove, lastTickStats.speedGain),
+				strafeDir: (bValidWishMove ? 1 : 0) * lastTickStats.strafeRight
+			});
+		}
 
 		const colorTuple = this.colorEnable
 			? this.getColorTuple(gainRatio, false) //strafeRight * yawRatio > 1)
@@ -106,6 +143,126 @@ class Synchronizer {
 				}
 				break;
 		}
+
+		if (this.shouldDraw()) this.draw();
+	}
+
+	static shouldDraw() {
+		return this.statMode !== 2 && this.graphMode > 0 && (this.displayMode === 1 || this.displayMode === 3);
+	}
+
+	static draw() {
+		this.panels.graph.Clear(this.altColor);
+		this.initializeGraph();
+
+		switch (this.displayMode) {
+			case 1:
+				this.drawHalfWidthRects();
+				break;
+			case 3:
+				this.drawIndicatorRects();
+				break;
+		}
+
+		if (this.dynamicEnable === 1) this.drawStrafeDirRects();
+	}
+
+	static drawHalfWidthRects() {
+		for (let i = 0; i < this.graphHistory.length; i++) {
+			const { ratio, flow, gainRatio, syncState } = this.graphHistory[i];
+
+			if (ratio <= 0) continue;
+
+			const dynamicRatio = flow === 1 ? ratio : 2 - ratio;
+
+			const pointA = this.getCanvasPoint(i, dynamicRatio / 2);
+			const pointB = this.getCanvasPoint(i + 1, dynamicRatio / 2);
+			const pointC = this.getCanvasPoint(i + 1, flow === 1 ? 0 : 1);
+			const pointD = this.getCanvasPoint(i, flow === 1 ? 0 : 1);
+
+			const points =
+				flow === 1
+					? [...pointA, ...pointB, ...pointC, ...pointD]
+					: [...pointA, ...pointD, ...pointC, ...pointB];
+
+			const colorTuple = this.getColorTuple(gainRatio, false) ?? COLORS.NEUTRAL;
+
+			const color = this.colorEnable ? colorTuple : GRAPH_COLORS[syncState];
+
+			this.panels.graph.DrawPoly(4, points, color);
+		}
+	}
+
+	static drawIndicatorRects() {
+		for (let i = 0; i < this.graphHistory.length; i++) {
+			const { ratio, flow, gainRatio, syncState } = this.graphHistory[i];
+
+			if (ratio <= 0) continue;
+
+			const offset = Math.min(Math.max(0.5 - 0.5 * ratio, 0), 1);
+			let left = (offset * this.indicatorPercentage) / 100;
+			let right = left + 0.1;
+
+			if (flow === -1) {
+				left = 1 - left;
+				right = 1 - right;
+			}
+
+			const pointA = this.getCanvasPoint(i, right);
+			const pointB = this.getCanvasPoint(i + 1, right);
+			const pointC = this.getCanvasPoint(i + 1, left);
+			const pointD = this.getCanvasPoint(i, left);
+
+			const points =
+				flow === 1
+					? [...pointA, ...pointB, ...pointC, ...pointD]
+					: [...pointA, ...pointD, ...pointC, ...pointB];
+
+			const color = this.colorEnable ? this.getColorTuple(gainRatio, false)[1] : GRAPH_COLORS[syncState];
+
+			this.panels.graph.DrawPoly(4, points, color);
+		}
+	}
+
+	static drawStrafeDirRects() {
+		for (let i = 0; i < this.graphHistory.length; i++) {
+			const { strafeDir } = this.graphHistory[i];
+
+			if (!strafeDir) continue;
+
+			const offset = strafeDir === 1 ? this.strafeDirPercentage : 1 - this.strafeDirPercentage;
+
+			const pointA = this.getCanvasPoint(i, offset);
+			const pointB = this.getCanvasPoint(i + 1, offset);
+			const pointC = this.getCanvasPoint(i + 1, strafeDir === 1 ? 0 : 1);
+			const pointD = this.getCanvasPoint(i, strafeDir === 1 ? 0 : 1);
+
+			const points =
+				strafeDir === 1
+					? [...pointA, ...pointB, ...pointC, ...pointD]
+					: [...pointA, ...pointD, ...pointC, ...pointB];
+
+			this.panels.graph.DrawPoly(4, points, 'rgba(87, 200, 255, 0.9)');
+		}
+	}
+
+	static getSyncState(bValidWishMove, speedGain) {
+		if (!bValidWishMove) {
+			return SYNC_STATES.NOMOVE;
+		} else if (speedGain > 0) {
+			return SYNC_STATES.SYNCED;
+		} else if (speedGain < 0) {
+			return SYNC_STATES.LOSS;
+		} else {
+			return SYNC_STATES.UNSYNCED;
+		}
+	}
+
+	static getCanvasPoint(i, x) {
+		const xScale = this.width;
+		const yScale = this.height / this.graphHistory.length;
+
+		return [x * xScale, i * yScale];
 	}
 
 	static onJump() {
@@ -148,6 +305,11 @@ class Synchronizer {
 			else if (ratio > 0) return lerpColorTuples(COLORS.LOSS, COLORS.SLOW, (ratio - 0.25) / 0.55);
 			else if (ratio > -5) return lerpColorTuples(COLORS.LOSS, COLORS.STOP, Math.abs(ratio) / 5);
 		}
+	}
+
+	static parseColorString(colorString) {
+		const rgbaValues = colorString.split(' ').join(', ');
+		return `rgba(${rgbaValues})`;
 	}
 
 	static wrapValueToRange(value, min, max, bShouldTrackWrap) {
@@ -210,6 +372,11 @@ class Synchronizer {
 		buffer.shift();
 	}
 
+	static addToBufferFront(buffer, value) {
+		buffer.unshift(value);
+		buffer.pop();
+	}
+
 	static getBufferedSum(history) {
 		return history.reduce((sum, element) => sum + element, 0);
 	}
@@ -241,10 +408,14 @@ class Synchronizer {
 			case 1: // "Half-width throttle"
 				for (const segment of this.panels.segments) segment.style.backgroundColor = this.altColor;
 				this.panels.needle.style.visibility = 'visible';
+				this.setGraphMode(this.graphMode);
 				break;
 			case 2: // "Full-width throttle"
 				for (const segment of this.panels.segments) segment.style.backgroundColor = this.altColor;
 				this.panels.needle.style.visibility = 'collapse';
+				this.panels.wrapper.style.transform = 'scaleX(1) rotateZ(0deg)';
+				this.panels.graphContainer.style.visibility = 'collapse';
+				this.panels.background.style.borderRadius = '8px';
 				break;
 			case 3: // "Strafe indicator"
 				this.panels.segments[1].style.width = 100 - this.indicatorPercentage + '%';
@@ -252,12 +423,16 @@ class Synchronizer {
 				this.panels.segments[3].style.width = 50 + '%';
 				for (const segment of this.panels.segments) segment.style.backgroundColor = this.altColor;
 				this.panels.needle.style.visibility = 'visible';
+				this.setGraphMode(this.graphMode);
 				break;
 			case 4: // "Synchronizer"
 				for (const segment of this.panels.segments) {
 					segment.style.width = this.maxSegmentWidth + '%';
 				}
 				this.panels.needle.style.visibility = 'collapse';
+				this.panels.wrapper.style.transform = 'scaleX(1) rotateZ(0deg)';
+				this.panels.graphContainer.style.visibility = 'collapse';
+				this.panels.background.style.borderRadius = '8px';
 				break;
 		}
 	}
@@ -282,6 +457,17 @@ class Synchronizer {
 		this.yawRatioHistory = this.initializeBuffer(this.interpFrames);
 	}
 
+	static setGraphBufferLength(newBufferLength) {
+		const bufferLength = newBufferLength ?? DEFAULT_GRAPH_BUFFER_LENGTH;
+
+		this.graphHistory = this.initializeBuffer(bufferLength);
+	}
+
+	static initializeGraph() {
+		this.height = this.panels.graph.actuallayoutheight / this.panels.graph.actualuiscale_y;
+		this.width = this.panels.graph.actuallayoutwidth / this.panels.graph.actualuiscale_x;
+	}
+
 	static setMinSpeed(newMinSpeed) {
 		this.minSpeed = newMinSpeed ?? DEFAULT_MIN_SPEED;
 	}
@@ -295,11 +481,33 @@ class Synchronizer {
 		this.StatColorEnable = newColorMode ?? DEFAULT_SETTING_OFF;
 	}
 
+	static setGraphMode(newMode) {
+		this.graphMode = newMode ?? DEFAULT_GRAPH_MODE;
+		switch (this.graphMode) {
+			case 0: // "Off"
+				this.panels.graphContainer.style.visibility = 'collapse';
+				this.panels.background.style.borderRadius = '8px';
+				this.panels.wrapper.style.transform = 'scaleX(1) rotateZ(0deg)';
+				break;
+			case 1: // "On"
+				this.panels.graphContainer.style.visibility = 'visible';
+				this.panels.background.style.borderRadius = '8px 8px 0px 0px';
+				this.panels.wrapper.style.transform = 'scaleX(1) rotateZ(0deg)';
+				break;
+			case 2: // "Rotated"
+				this.panels.graphContainer.style.visibility = 'visible';
+				this.panels.background.style.borderRadius = '8px 8px 0px 0px';
+				this.panels.wrapper.style.transform = 'scaleX(-1) rotateZ(90deg)';
+				break;
+		}
+	}
+
 	static NaNCheck(val, def) {
 		return Number.isNaN(Number(val)) ? def : val;
 	}
 
 	static initializeSettings() {
+		this.setGraphMode();
 		this.setDisplayMode(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_mode'));
 		this.setColorMode(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_color_enable'));
 		this.setDynamicMode(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_dynamic_enable'));
@@ -308,6 +516,7 @@ class Synchronizer {
 		this.setMinSpeed(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_min_speed'));
 		this.setStatMode(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_stat_mode'));
 		this.setStatColorMode(GameInterfaceAPI.GetSettingFloat('mom_hud_synchro_stat_color_enable'));
+		this.setGraphBufferLength();
 	}
 
 	static {
